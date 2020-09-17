31f7c1b06a3cf3d0eb6b3d94bb039b45626b2606
Tanner Yould <48941951+tanneryould@users.noreply.github.com>
2020-09-17 00:02:55 -0700

    updated formatting

--- cpp/number_of_good_pairs.cpp
+++ cpp/number_of_good_pairs.cpp
@@ -3,22 +3,12 @@
 https://leetcode.com/problems/number-of-good-pairs/
 
 1512. Number of Good Pairs
-Easy
 
-473
-
-35
-
-Add to List
-
-Share
 Given an array of integers nums.
 
 A pair (i,j) is called good if nums[i] == nums[j] and i < j.
 
 Return the number of good pairs.
-
- 
 
 Example 1:
 
@@ -41,6 +31,8 @@ Constraints:
 1 <= nums.length <= 100
 1 <= nums[i] <= 100
 
+
+Solution results:
 Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of Good Pairs.
 Memory Usage: 7.5 MB, less than 18.23% of C++ online submissions for Number of Good Pairs.
 
